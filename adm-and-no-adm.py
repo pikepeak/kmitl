76451b9144aa7a639eb7ba37c3bbce42e81a9a91
mikanchanss <somddt@gmail.com>
2015-12-14 00:55:37 +0700

    update code 13 final

--- adm-and-no-adm.py
+++ adm-and-no-adm.py
@@ -1,36 +1,43 @@
 import csv
-
-with open('สมุดงาน1.csv', newline="", encoding='utf-8') as aaa:
-    a = csv.reader(aaa)
-    total = list(a)
-
-adm = 0
-noadm = 0
-lis_data = []
-for i in total:
-    adm += int(i[2])
-    noadm += int(i[3])
-    lis_data.append(int(i[2]))
-    lis_data.append(int(i[3]))
-
 import plotly.plotly as py
 import plotly.graph_objs as go
-fig = {
-  "data": [
-    {
-      "values": [adm, noadm],
-      "labels": [
-        "Admission",
-        "NotAdmission",
-      ],
-      "domain": {"x": [0, .45]},
-      "type": "pie",
-      'textinfo':'value+percent'
-    },     
-    {
-      "values": lis_data,
-      "labels": [
-        "แอดมิชชั่นคณะวิศวกรรมศาสตร์",
+def datainput():
+    """csvdata input to list"""
+    with open('สมุดงาน1.csv', newline="", encoding = 'utf-8') as data:
+        list1 = csv.reader(data)
+        listdata = list(list1)
+    list_var(listdata)
+
+def list_var(listdata):
+    """list to var"""
+    adm = 0
+    noadm = 0
+    lis_data = []
+    for i in listdata:
+        adm += int(i[2])
+        noadm += int(i[3])
+        lis_data.append(int(i[2]))
+        lis_data.append(int(i[3]))
+    graph(adm, noadm, lis_data)
+
+def graph(adm, noadm, lis_data):
+    """printgraph"""
+    fig = {
+      "data": [
+        {
+          "values": [adm, noadm],
+          "labels": [
+            "Admission",
+            "NotAdmission",
+          ],
+          "domain": {"x": [0, .45]},
+          "type": "pie",
+          'textinfo':'value+percent'
+        },     
+        {
+          "values": lis_data,
+          "labels": [
+            "แอดมิชชั่นคณะวิศวกรรมศาสตร์",
         "ไม่แอดมิชชั่นคณะวิศวกรรมศาสตร์",
         "แอดมิชชั่นสถาปัตยกรรมศาสตร์",
         "ไม่แอดมิชชั่นสถาปัตยกรรมศาสตร์",
@@ -44,16 +51,24 @@ fig = {
         "ไม่แอดมิชชั่นคณะอุตสาหกรรมเกษตร",
         "แอดมิชชั่นคณะอื่นๆ",
         "ไม่แอดมิชชั่นคณะอื่นๆ"
+          ],
+          "text":"แอดและไม่แอดของแต่ละคณะ",
+          "domain": {"x": [.50, 0.98]},
+          "type": "pie",
+          'textinfo':'value+percent',
+          'sort':False,
+          'pull':.08,
+          'marker': {'colors': ['rgb(0, 255, 0)', 'rgb(0, 220, 0)',
+                            'rgb(255, 0, 0)', 'rgb(220, 0 ,0)',
+                            'rgb(255, 128, 0)', 'rgb(220 , 128, 0)',
+                            'rgb(255, 255, 0)', 'rgb(220, 220, 0)',
+                            'rgb(128, 255, 0)', 'rgb(128, 220, 0)',
+                            'rgb(0, 255, 255)', 'rgb(0, 220, 220)',
+                            'rgb(255, 0, 255)', 'rgb(220, 0, 220)']}
+        },
       ],
-      "text":"แอดและไม่แอดของแต่ละคณะ",
-      "domain": {"x": [.45, 0.90]},
-      "type": "pie",
-      'textinfo':'value+percent'
-    },     
-  ],
-  "layout": {
-        "title":'จำนวนที่เเอดมิชชั่นเเละไม่เเอดมิชชั่นของเเต่ละคณะเเละโดยรวม',
-        }
-}
+      "layout": {"title":'จำนวนที่เเอดมิชชั่นเเละไม่เเอดมิชชั่นของเเต่ละคณะเเละโดยรวม'}
+    }
 
-url = py.plot(fig, filename='admission and notadmission')
+    url = py.plot(fig, filename='admission and notadmission')
+datainput()
